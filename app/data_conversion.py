727a28cbdf55c1d3a72f6230282650074ab8bf48
Lucas <lucasnholler@gmail.com>
2026-03-23 13:12:12 -0600

    updating from main

--- app/data_conversion.py
+++ app/data_conversion.py
@@ -4,12 +4,6 @@ Script to  transform .csv to .parquet and then pipe data to Google Cloud Storage
 from models.sales_data import SalesData
 from models.logger import get_logger
 from dotenv import load_dotenv
-"""
-Script to  transform .csv to .parquet and then pipe data to Google Cloud Storage (GCS)
-"""
-from models.sales_data import SalesData
-from models.logger import get_logger
-from dotenv import load_dotenv
 import pandas as pd
 import csv
 import os
@@ -90,80 +84,5 @@ def _read_and_clean_data() -> pd.DataFrame:
                 break
     return df
 
-if __name__ == "__main__":
-    main()
-import csv
-logger = get_logger(__name__)
-def main():
-    """Creates and uploads .parquet file to GCS
-    """
-    load_dotenv() # get environment variables
-
-    # uri to the GCS bucket location
-    gcs_uri = os.getenv("GCS_URI")
-    # path to the file with your GCS credentials
-    gc_auth = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-
-    if gcs_uri is None:
-        logger.error("GCS_URI environment variable not found. Check .env file and README.md for setup help.")
-    if gc_auth is None:
-        logger.error("GOOGLE_APPLICATION_CREDENTIALS environment variable not found. Check .env file and README.md for setup help.")
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = gc_auth
-    data = _read_and_clean_data()
-
-    # upload .parquet to Google Cloud Storage
-    try:
-        data.to_parquet(gcs_uri, engine='pyarrow')
-        logger.info(f"Parquet saved to {gcs_uri}")
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-
-def _validate_csv(input_path: str) -> pd.DataFrame:
-    """ Helper function to validate a .csv file row by row. Returned valid records as a DataFrame and logs invalid records to error log
-
-    Args:
-        input_path (str): .csv file path
-
-    Returns:
-        pd.DataFrame: DataFrame of valid records
-    """
-    with open(input_path, newline='') as infile:
-        reader = csv.reader(infile)
-        next(reader)  # skip header row
-        rows = []
-
-        for row in reader:
-            try:
-                record = SalesData.convert_csv_types(row) # convert fields to types of SalesData model
-                rows.append(record.to_row()) # adds valid record to list
-
-            except Exception as e:
-                logger.error(f"Error: {e}\n Record: {row}") # adds invalid records to log
-
-    return pd.DataFrame(rows, columns=SalesData.columns)
-
-def _read_and_clean_data() -> pd.DataFrame:
-    """ Helper function to read .csv files from ../data/, cleans the data and return as a DataFrame
-
-    Returns:
-        pd.DataFrame: .csv data cleaned and validated
-    """
-    directory_path = '../data/'
-    df = pd.DataFrame()
-    # combine multiple .csv files
-    with os.scandir(directory_path) as batches:
-        for batch in batches:
-            if batch.name.endswith('.csv'):
-                logger.info(f"Validating file: {batch.path}")
-                cleaned_batch = _validate_csv(batch.path)
-
-                if df.empty:
-                    df = cleaned_batch
-                else:
-                    df = pd.concat([df, cleaned_batch])
-            else:
-                print(f"{batch.name} is not a csv")
-                break
-    return df
 if __name__ == "__main__":
     main()
